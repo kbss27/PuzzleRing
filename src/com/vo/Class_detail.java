d59e81ff614e8dde2a660fd8683234dc08dd1463
Jeonghwa <purify20@gmail.com>
2016-04-07 18:42:29 +0900

    create project 부분 연결.

---
+++ src/com/vo/Class_detail.java
@@ -0,0 +1,22 @@
+package com.vo;
+
+public class Class_detail {
+
+	private String class_am, class_name;
+
+	public String getClass_am() {
+		return class_am;
+	}
+
+	public void setClass_am(String class_am) {
+		this.class_am = class_am;
+	}
+
+	public String getClass_name() {
+		return class_name;
+	}
+
+	public void setClass_name(String class_name) {
+		this.class_name = class_name;
+	}
+}
---
+++ src/com/vo/Method_detail.java
@@ -0,0 +1,45 @@
+package com.vo;
+
+public class Method_detail {
+	private String method_am, method_type, method_modifier, method_name, class_name;
+
+	public String getMethod_am() {
+		return method_am;
+	}
+
+	public void setMethod_am(String method_am) {
+		this.method_am = method_am;
+	}
+
+	public String getMethod_type() {
+		return method_type;
+	}
+
+	public void setMethod_type(String method_type) {
+		this.method_type = method_type;
+	}
+
+	public String getMethod_modifier() {
+		return method_modifier;
+	}
+
+	public void setMethod_modifier(String method_modifier) {
+		this.method_modifier = method_modifier;
+	}
+
+	public String getMethod_name() {
+		return method_name;
+	}
+
+	public void setMethod_name(String method_name) {
+		this.method_name = method_name;
+	}
+
+	public String getClass_name() {
+		return class_name;
+	}
+
+	public void setClass_name(String class_name) {
+		this.class_name = class_name;
+	}
+}
